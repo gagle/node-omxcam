d7ae85fdfcc27427e747a35ca6e65ca1ac80d8a0
Gabriel Llamas <gabriel_llamas_llopis@yahoo.es>
2014-07-26 13:52:28 +0200

    added rgb

---
+++ examples/video/rgb.js
@@ -0,0 +1,43 @@
+"use strict";
+
+var fs = require ("fs");
+var omxcam = require ("../../lib");
+
+var record = function (filename, settings, totalFrames){
+  var fd = fs.openSync (filename, "w");
+  var video = omxcam.video (settings);
+  
+  video.start ();
+  
+  var buffer;
+  var current = 0;
+  var length;
+  var stop = false;
+  
+  //TODO use video.settings()
+  var total = settings.width*settings.height*3*totalFrames;
+  
+  while (!stop){
+    buffer = video.read ();
+    current += buffer.length;
+    length = buffer.length;
+    
+    if (current >= total){
+      length -= current - total;
+      stop = true;
+    }
+    
+    fs.writeSync (fd, buffer, 0, length, null);
+  }
+  
+  video.stop ();
+  fs.closeSync (fd);
+};
+
+try{
+  record ("video.rgb",
+      { format: omxcam.FORMAT_RGB888, width: 640, height: 480 }, 10);
+  console.log ("ok");
+}catch (error){
+  console.error (error);
+}
---
+++ examples/video/yuv.js
@@ -0,0 +1,77 @@
+"use strict";
+
+var fs = require ("fs");
+var omxcam = require ("../../lib");
+
+var record = function (filename, settings, totalFrames){
+  var fd = fs.openSync (filename, "w");
+  var video = omxcam.video (settings);
+  
+  //TODO use video.settings()
+  var planes = omxcam.yuvPlanes (settings.width, settings.height);
+  var planesSlice = omxcam.yuvPlanesSlice (settings.width);
+  var frameSize = planes.offsetV + planes.lengthV;
+  
+  video.start ();
+  
+  var buffer;
+  var slice;
+  var yBuffers = [];
+  var uBuffers = [];
+  var vBuffers = [];
+  var current = 0;
+  var frames = 0;
+  
+  while (true){
+    buffer = video.read ();
+    current += buffer.length;
+    
+    slice = new Buffer (planesSlice.lengthY);
+    buffer.copy (slice, 0, planesSlice.offsetY,
+        planesSlice.offsetY + planesSlice.lengthY);
+    yLength += slice.length;
+    yBuffers.push (slice);
+    
+    slice = new Buffer (planesSlice.lengthU);
+    buffer.copy (slice, 0, planesSlice.offsetU,
+        planesSlice.offsetU + planesSlice.lengthU);
+    uLength += slice.length;
+    uBuffers.push (slice);
+    
+    slice = new Buffer (planesSlice.lengthV);
+    buffer.copy (slice, 0, planesSlice.offsetV,
+        planesSlice.offsetV + planesSlice.lengthV);
+    vLength += slice.length;
+    vBuffers.push (slice);
+    
+    if (current === frameSize){
+      //An entire YUV frame has been received
+      current = 0;
+      
+      buffer = Buffer.concat (yBuffers, planes.lengthY);
+      yBuffers.length = 0;
+      fs.writeSync (fd, buffer, 0, buffer.length, null);
+      
+      buffer = Buffer.concat (uBuffers, planes.lengthU);
+      uBuffers.length = 0;
+      fs.writeSync (fd, buffer, 0, buffer.length, null);
+      
+      buffer = Buffer.concat (vBuffers, planes.lengthV);
+      vBuffers.length = 0;
+      fs.writeSync (fd, buffer, 0, buffer.length, null);
+      
+      if (++frames === totalFrames) break;
+    }
+  }
+  
+  video.stop ();
+  fs.closeSync (fd);
+};
+
+try{
+  record ("video.yuv",
+      { format: omxcam.FORMAT_YUV420, width: 640, height: 480 }, 10);
+  console.log ("ok");
+}catch (error){
+  console.error (error);
+}
